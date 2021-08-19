4769bb8bea202c5f1c388fd42f29f1dab5d59987
Drakon <tnlfl288@gamail.com>
2021-08-19 21:27:53 +0900

    크라켄 업로드 테스트 2

---
+++ Unity class 29/Assets/hou.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hou : MonoBehaviour
+{
+
+    int a = 3;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}
